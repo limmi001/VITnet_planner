3af45ce00990a385ca5cf20547f6342fd4abff06
limmi001 <1586926281@qq.com>
2025-12-26 19:37:36 +0800

    add vitnet and sub modules

--- YOPO/policy/models/backbone.py
+++ YOPO/policy/models/backbone.py
@@ -1,7 +1,67 @@
 import time
 import torch
 import torch.nn
+import torch.nn.functional as F
+import torch.nn.utils.spectral_norm as spectral_norm
 from policy.models.resnet import resnet18
+from ViTsubmodules import *
+
+def refine_inputs(X):
+
+    # fill quaternion rotation if not given
+    # make it [1, 0, 0, 0] repeated with numrows = X[0].shape[0]
+    if X[2] is None:
+        # X[2] = torch.Tensor([1, 0, 0, 0]).float()
+        X[2] = torch.zeros((X[0].shape[0], 4)).float().to(X[0].device)
+        X[2][:, 0] = 1
+
+    # if input depth images are not of right shape, resize
+    if X[0].shape[-2] != 60 or X[0].shape[-1] != 90:
+        X[0] = F.interpolate(X[0], size=(60, 90), mode='bilinear')
+
+    return X
+
+
+class LSTMNetVIT(torch.nn.Module):
+    """
+    ViT+LSTM Network 
+    Num Params: 3,563,663   
+    """
+    def __init__(self):
+        super().__init__()
+        self.encoder_blocks = torch.nn.ModuleList([
+            MixTransformerEncoderLayer(1, 32, patch_size=7, stride=4, padding=3, n_layers=2, reduction_ratio=8, num_heads=1, expansion_factor=8),
+            MixTransformerEncoderLayer(32, 64, patch_size=3, stride=2, padding=1, n_layers=2, reduction_ratio=4, num_heads=2, expansion_factor=8)
+        ])
+
+        self.decoder = spectral_norm(torch.nn.Linear(4608, 512))
+        self.lstm = (torch.nn.LSTM(input_size=517, hidden_size=128,
+                         num_layers=3, dropout=0.1))
+        self.nn_fc2 = spectral_norm(torch.nn.Linear(128, 3))
+
+        self.up_sample = torch.nn.Upsample(size=(16,24), mode='bilinear', align_corners=True)
+        self.pxShuffle = torch.nn.PixelShuffle(upscale_factor=2)
+        self.down_sample = torch.nn.Conv2d(48,12,3, padding = 1)
+
+    def forward(self, X):
+
+        X = refine_inputs(X)
+
+        x = X[0]
+        embeds = [x]
+        for block in self.encoder_blocks:
+            embeds.append(block(embeds[-1]))        
+        out = embeds[1:]
+        out = torch.cat([self.pxShuffle(out[1]),self.up_sample(out[0])],dim=1) 
+        out = self.down_sample(out)
+        out = self.decoder(out.flatten(1))
+        out = torch.cat([out, X[1]/10, X[2]], dim=1).float()
+        if len(X)>3:
+            out,h = self.lstm(out, X[3])
+        else:
+            out,h = self.lstm(out)
+        out = self.nn_fc2(out)
+        return out, h
 
 
 # input: [1, 96, 160]
